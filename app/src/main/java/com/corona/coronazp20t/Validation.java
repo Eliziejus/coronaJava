bf9940eec85ba96889b2d5a714bc5a26b8347481
Eliziejus <capaseliziejus@gmail.com>
2020-11-06 15:12:55 +0200

    project

---
+++ app/src/main/java/com/corona/coronazp20t/Validation.java
@@ -0,0 +1,29 @@
+package com.corona.coronazp20t;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    public static final String USERNAME_REGEX_PATTERN="^[A-Za-z]{3,20}$";
+    public static final String PASSWORD_REGEX_PATTERN="^[A-Za-z0-9(.!@_)]{5,20}$";
+    public static final String EMAIL_REGEX_PATTERN="^[A-Za-z0-9(@._-)]{10,50}";
+
+    public static boolean isValidUsername(String username){
+        Pattern pattern=Pattern.compile(USERNAME_REGEX_PATTERN);//Kuriamas sablonas pagal musu stringa
+        Matcher matcher=pattern.matcher(username);//Palyginamas sablonas su vartotojo ivestais duomenimis
+
+        return matcher.find();//Grazina true jeigu atitinka, false priesingu atveju
+    }
+    public static  boolean isValidPassword(String password){
+        Pattern pattern=Pattern.compile(PASSWORD_REGEX_PATTERN);
+        Matcher matcher=pattern.matcher(password);
+
+        return matcher.find();
+    }
+    public static boolean isValidEmail(String email){
+        Pattern pattern=Pattern.compile(EMAIL_REGEX_PATTERN);
+        Matcher matcher=pattern.matcher(email);
+
+        return matcher.find();
+    }
+}
